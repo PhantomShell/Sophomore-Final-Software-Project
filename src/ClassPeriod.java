4b247f704feb16c8fc1389e3159bde5afab884e0
Jagan Prem <notjagan@gmail.com>
2016-04-12 10:41:07 -0400

    Created the Java Project files in the repository and added the basic structure of the ClassPeriod class.

---
+++ src/ClassPeriod.java
@@ -0,0 +1,24 @@
+
+public class ClassPeriod {
+	int room;
+	int classSize;
+	String teacher;
+	
+	public ClassPeriod(int room, int classSize, String teacher) {
+		this.room = room;
+		this.classSize = classSize;
+		this.teacher = teacher;
+	}
+	
+	public int getRoom() {
+		return room;
+	}
+	
+	public int getClassSize() {
+		return classSize;
+	}
+	
+	public String getTeacher() {
+		return teacher;
+	}
+}
