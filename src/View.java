19f20aa1e85da05aabbb59a4a607d37681fafa3d
Jagan Prem <notjagan@gmail.com>
2016-04-28 00:02:26 -0400

    Began work on JavaFX code and code for PDF previewing. Also added jars for PDF-renderer and PDFbox, which render and edit PDF documents, respectively.

--- src/View.java
+++ src/View.java
@@ -1,13 +1,19 @@
+import java.io.IOException;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
- 
+
 public class View extends Application {
-    public static void main(String[] args) {
-        launch(args);
-    }
-    
-    @Override
-    public void start(Stage primaryStage) {
-        
-    }
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		final Parent parent = FXMLLoader.load(getClass().getResource("PdfViewer.fxml"));
+		primaryStage.setScene(new Scene(parent, 800, 600));
+		primaryStage.setMaximized(true);
+		primaryStage.show();
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
